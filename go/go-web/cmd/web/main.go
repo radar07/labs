080f2e7613060916ad074bcce6e55f6d8a2046c1
Pranav RK <pranavrk7@gmail.com>
2024-08-28 17:17:32 +0530

    go-web: add flags

--- go/go-web/cmd/web/main.go
+++ go/go-web/cmd/web/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
 	// If `NewServeMux` is specified it will use `DefaultServeMux`
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
@@ -15,5 +18,5 @@ func main() {
 
 	// http.HandleFunc("/posts", posts)
 	// http.HandleFunc("/posts?id=3", getPost)
-	log.Fatal(http.ListenAndServe(":8090", mux))
+	log.Fatal(http.ListenAndServe(*addr, mux))
 }
