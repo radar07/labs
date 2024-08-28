baa154157b7c08e5b56ab69dba64a856e345c963
Pranav RK <pranavrk7@gmail.com>
2024-08-28 14:05:26 +0530

    go-web: add changes

--- go/go-web/cmd/web/handlers.go
+++ go/go-web/cmd/web/handlers.go
@@ -8,13 +8,18 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-	log.Println(r.URL.Query())
 	if r.URL.Path != "/" {
 		http.Error(w, "Page not found", 404)
 		return
 	}
 
-	ts, err := template.ParseFiles("./ui/html/home.page.tmpl")
+	files := []string{
+		"./ui/html/home.page.tmpl",
+		"./ui/html/base.layout.tmpl",
+		"./ui/html/footer.partial.tmpl",
+	}
+
+	ts, err := template.ParseFiles(files...)
 	if err != nil {
 		log.Println(err.Error())
 		return
--- go/go-web/cmd/web/main.go
+++ go/go-web/cmd/web/main.go
@@ -10,6 +10,9 @@ func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 
+	// Serve Static Files
+	mux.Handle("/static/", http.StripPrefix("/static", http.FileServer(http.Dir("./ui/static"))))
+
 	// http.HandleFunc("/posts", posts)
 	// http.HandleFunc("/posts?id=3", getPost)
 	log.Fatal(http.ListenAndServe(":8090", mux))
