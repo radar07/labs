3f6269d132559ed88e99411ce01bd3cfef545212
Pranav RK <pranavrk7@gmail.com>
2024-08-29 20:39:45 +0530

    go-web: add pg db driver

--- go/go-web/cmd/web/main.go
+++ go/go-web/cmd/web/main.go
@@ -1,16 +1,28 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/lib/pq"
 )
 
 type application struct {
 	errorLog *log.Logger
 	infoLog  *log.Logger
 }
+
+const (
+	host     = "localhost"
+	port     = "5432"
+	user     = "pranav"
+	password = "pranav"
+	dbname   = "web"
+)
 
 func main() {
 	file, err := os.OpenFile("/tmp/info.log", os.O_RDWR|os.O_CREATE, 0666)
@@ -36,6 +48,28 @@ func main() {
 		Handler:  app.routes(),
 	}
 
+	conn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	db, err := openDB(conn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	defer db.Close()
+
 	infoLog.Printf("Listening on port", *addr)
 	errorLog.Fatal(srv.ListenAndServe())
+}
+
+func openDB(dbconn string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", dbconn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
